471dd17d61f4a175e085400cd0189d1786406d80
adieltapari <adiel.tapari@gmail.com>
2022-12-22 18:58:11 -0300

    add icon and link cv

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -2,6 +2,8 @@ import { FC } from 'react';
 import styles from './Contact.module.scss';
 import { AiFillGithub } from 'react-icons/ai';
 import { BsLinkedin } from 'react-icons/bs';
+import { TbReport } from 'react-icons/tb';
+
 import Link from 'next/link';
 import BaseText from '../common/BaseText';
 
@@ -9,6 +11,9 @@ const Contact: FC = () => {
   return (
     <div id="contact" className={styles.contactContainer}>
       <div className={styles.iconContainer}>
+        <Link href="https://docs.google.com/document/d/1XNW5Ftjun4v8lDd8tV92u8idvZiB-70VTAE0BuRZAIM/edit?usp=sharing">
+          <TbReport size={30} />
+        </Link>
         <Link href="https://github.com/adieltapari">
           <AiFillGithub size={30} />
         </Link>
